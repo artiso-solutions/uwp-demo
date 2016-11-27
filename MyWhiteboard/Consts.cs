ba0b6b7fae580f374fa23d7a40784bdfc80f63a7
Matthias Rink <matthias.rink@live.com>
2016-11-27 23:24:17 +0100

    removed additional connection strings

--- MyWhiteboard/Consts.cs
+++ MyWhiteboard/Consts.cs
@@ -2,10 +2,8 @@
 {
     public class Consts
     {
-        public const string ApiControllerBaseUrl = "http://192.168.178.66:81/api/review/";
-        //public const string ApiControllerBaseUrl = "http://localhost:36718/api/review/";
+        public const string ApiControllerBaseUrl = "http://localhost:36718/api/review/";
 
-        public const string SignalRUrl = "http://192.168.178.66:81/signalr";
-        //public const string SignalRUrl = "http://localhost:36718/signalr";
+        public const string SignalRUrl = "http://localhost:36718/signalr";
     }
 }
